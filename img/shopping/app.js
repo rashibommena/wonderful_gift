0857f20d66d003b3b69cf677834b5a6e77b1ea93
rashi <bommenarashi@gmail.com>
2024-01-01 23:14:27 +0530

    initial commit

---
+++ img/shopping/app.js
@@ -0,0 +1,77 @@
+// data types : string, number, null, boolean, undefined, object, array
+
+
+//var v= 10;
+ //let l = 15;
+ //const c =20;
+   //console.log(v);
+
+ //'a' == 'b'  
+//console.log("HELLO JS");
+
+//console.log( 5 + 9 );
+//console.log(1+4, "HI", true)
+
+//alert("HELLO WORLD") alert is going to print out something to the user,
+//prompt("worald") prompt is going to accept an argument
+
+//prompt("please enter a number")
+//console.log("HELLO FROM OUR FIST JS FILE!!!");
+//var nbm = 6+3;
+//let word = "hello";
+
+//let rating =3;
+//if (rating === 3){
+  //console.log("YOU ARE A SUPERSTAR! VILAS ANNAAA");
+//}
+//console.log("BEFORE CONDITIONAL")
+//if (1 + 1 ===2){
+  //console.log("MATH STILL WORKS!")
+//}
+//console.log("AFTER CONDITIONAL")
+
+//function isEven(num){
+     
+  //if(num%2 === 0){
+    //console.log("even");
+  //}
+//}
+
+//const dayOfWeek ='tuesday';
+
+//if(dayOfWeek === 'Monday'){
+  //console.log("Good MONDAY!")
+//}else if(dayOfWeek === 'saturday'){
+  //console.log("SATURDAYS LOVE IT")
+//}else if(dayOfWeek === 'friday'){
+  //console.log('friday is a very good')
+//}
+
+ //0-5 - FREE
+ //5-10 - CHILD $10
+ //10-65 -ADULI $20
+ //65+ SENIOR $10
+
+//const password = prompt("please enter a new password");
+ //password must be 6+ characters
+ //if (password.length >= 6) {
+  //console.log("LONG ENOUGH PASSWORD!")
+ //} else{
+  //console.log("PASSWORD TOO SHORT! Must be 6+ characters")
+ //}
+ //password cannot include space
+
+
+
+ //const age = 9;
+
+//if (age < 5) {
+  //console.log("You are a baby. You get in for free!")
+//} else if (age < 10) {
+  //console.log("You are a child. You pay $10")
+//} else if (age < 65) {
+  //console.log("You are an aduit. You pay $20")
+//}
+
+
+const userInput = prompt("enter something");  
